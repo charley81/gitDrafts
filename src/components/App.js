133363535ae3cfee57169280a22a9822fadfd0b4
ChrisKnowsDev <chrisharley81@gmail.com>
2020-06-29 14:24:21 -0400

    css setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 import Brewery from "./Brewery";
-import { css } from "@emotion/core";
 
 const App = () => {
   return (
-    <div
-      css={css`
-        background-color: red;
-      `}
-    >
+    <div>
       <h1>gitDrafts</h1>
       <Brewery name="Jailhouse Brewery" location="Hampton, Ga" type="micro" />
       <Brewery name="Arches Brewing" location="Hapeville, Ga" type="micro" />
