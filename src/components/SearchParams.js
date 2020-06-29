0d8c0ab5e4bee5db9ac2605b54db635e609dce26
ChrisKnowsDev <chrisharley81@gmail.com>
2020-06-29 16:14:09 -0400

    created location hook and set inital state

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Atlanta, GA";
+  const [location, setLocation] = useState("Atlanta, GA");
 
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           location
-          <input type="text" id='location' value={location} placeholder="location"/>
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
