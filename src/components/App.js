aa5c0f6325d9016c58655bd48263deabe1bc335d
ChrisKnowsDev <chrisharley81@gmail.com>
2020-06-29 14:13:48 -0400

    uninstalled emotion

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 import Brewery from "./Brewery";
+import { css } from "@emotion/core";
 
 const App = () => {
   return (
-    <div>
+    <div
+      css={css`
+        background-color: red;
+      `}
+    >
       <h1>gitDrafts</h1>
       <Brewery name="Jailhouse Brewery" location="Hampton, Ga" type="micro" />
       <Brewery name="Arches Brewing" location="Hapeville, Ga" type="micro" />
