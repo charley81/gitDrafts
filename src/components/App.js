8807290ebad71293f5e4e8a1fdd191c7b867ac2e
ChrisKnowsDev <chrisharley81@gmail.com>
2020-06-29 16:20:48 -0400

    added adittional rules for hooks to .eslintrc

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,11 @@ import { render } from "react-dom";
 import Header from "./Header";
 import SearchParams from "./SearchParams";
 
-
 const App = () => {
   return (
     <div className="app">
-      <Header/>
-      <SearchParams/>
+      <Header />
+      <SearchParams />
     </div>
   );
 };
