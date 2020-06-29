ab30d217590f33f192c1fadc31c2fe69306848f0
ChrisKnowsDev <chrisharley81@gmail.com>
2020-06-29 11:14:24 -0400

    prettier setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,22 @@
 const Brewery = () => {
   return React.createElement("div", {}, [
-    React.createElement('h1', {}, "Jailhouse Brewery"),
-    React.createElement('h2', {}, "Micro"),
-    React.createElement('h2', {}, "Hampton, GA"),
-  ])
-}
-
+    React.createElement("h1", {}, "Jailhouse Brewery"),
+    React.createElement("h2", {}, "Micro"),
+    React.createElement("h2", {}, "Hampton, GA"),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-      "div",
-      {id: "something-important"},
-      React.createElement("h1", {}, "Drink Me!"),
-      [
-        React.createElement(Brewery),
-        React.createElement(Brewery),
-        React.createElement(Brewery),
-      ]
-    )
-}
+  return React.createElement(
+    "div",
+    { id: "something-important" },
+    React.createElement("h1", {}, "Drink Me!"),
+    [
+      React.createElement(Brewery),
+      React.createElement(Brewery),
+      React.createElement(Brewery),
+    ]
+  );
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
