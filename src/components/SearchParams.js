14000d4dceae97c5c6085c441177dad6bc5dbdac
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-01 12:20:27 -0400

    removed unneeded search hook and setSearch in from submit

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 
@@ -20,7 +20,6 @@ const SearchParams = () => {
         autoComplete="off"
         onSubmit={(e) => {
           e.preventDefault();
-          setSearch(city);
           requestBreweries();
         }}
       >
