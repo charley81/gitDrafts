4fc9423650cb1036bfe561b69a07d422ed3baed0
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-01 10:17:54 -0400

    removed state/types import from SearchParams

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { states, types } from "../inputData";
 
 const SearchParams = () => {
   const [data, setData] = useState();
