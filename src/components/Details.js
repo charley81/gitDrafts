a6e83c3486b0e8e05957275ffa639a1ac2990f31
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-02 17:54:05 -0400

    installed google-maps-react

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import { connect } from "react-redux";
 
 function Details({ id }) {
   const [brewery, setBrewery] = useState({});
