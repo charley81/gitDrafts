f5e7f0c71363d6cd17fd74d88f355069a3aed2b0
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-02 11:13:04 -0400

    initial content for Details.js

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Brewery = ({
-  key,
   name,
   type,
   street,
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-function Details(props) {
+function Details({ id }) {
+  const [brewery, setBrewery] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchBrewery = async () => {
+      setIsLoading(true);
+      const result = await Axios(
+        `https://api.openbrewerydb.org/breweries/${id}`
+      );
+      setBrewery(result.data);
+      setIsLoading(false);
+    };
+    fetchBrewery();
+  }, [id]);
+
+  const { name, street, city, state, phone, website_url } = brewery;
+
   return (
     <div className="details">
-      <h1>Details</h1>
+      {isLoading ? (
+        <div className="loading">Loading ...</div>
+      ) : (
+        <div className="details-info">
+          <h1>{name}</h1>
+          {street ? <h3>{street}</h3> : <h3>No Street Listed</h3>}
+          {city ? <h3>{city}</h3> : <h3>No City Listed</h3>}
+          {phone ? <h3>{phone}</h3> : <h3>No Phone Listed</h3>}
+          {website_url ? (
+            <button type="button" value={website_url}>
+              View Website
+            </button>
+          ) : (
+            <h3>No website listed</h3>
+          )}
+        </div>
+      )}
     </div>
   );
 }
