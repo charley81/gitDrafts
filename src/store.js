674f0135740005327a08ee06e5163e7869442da1
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-04 12:01:34 -0400

    added comments to store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { createStore } from "redux";
 import reducer from "./reducers";
 
+// reducer is just a function. it takes in old state and gives you new state
 const store = createStore(
   reducer,
+  // lets you use redux dev tools
   typeof window === "object" &&
     typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
     ? window.__REDUX_DEVTOOLS_EXTENSION__()
